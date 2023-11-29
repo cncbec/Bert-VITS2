fe2825f3d364f8e7b35f6c701821adba3f227fe6
cncbec <54312958+cncbec@users.noreply.github.com>
2023-11-29 18:01:48 +0800

    添加生成filelists下的list文件
    添加生成filelists下的list文件

--- slice.py
+++ slice.py
@@ -1,9 +1,10 @@
 import os
+model_name = 'gxd'
+out_file = f"filelists/{model_name}.list" #此处更改人名（英文简称）
 
-out_file = f"filelists/gxd.list" #此处更改人名（英文简称）
 def process():
     with open(out_file,'w' , encoding="Utf-8") as wf:
-        ch_name = 'gxd' #此处更改人名（英文简称）
+        ch_name = '{model_name}' #此处更改人名（英文简称）
         ch_language = 'ZH'
         path = f"./raw/{ch_name}"
         files = os.listdir(path)
