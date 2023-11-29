e4ca979fc56ab594ca3901023faae043daed50ab
cncbec <54312958+cncbec@users.noreply.github.com>
2023-11-29 17:57:46 +0800

    Update clean_list.py
    添加数据集切分功能

--- clean_list.py
+++ clean_list.py
@@ -4,39 +4,31 @@ import librosa
 from scipy.io import wavfile
 import numpy as np
 import whisper
+import librosa  # Optional. Use any library you like to read audio files.
+import soundfile  # Optional. Use any library you like to write audio files.
+
+from slicer2 import Slicer
 
 a="gxd" #此处更改人名（英文简称）
 
-def split_long_audio(model, filepaths, save_dir="data_dir", out_sr=44100):
-    files=os.listdir(filepaths)
-    filepaths=[os.path.join(filepaths,i)  for i in files]
+def split_long_audio(filepaths, save_dir="data_dir", out_sr=44100):
+  audio, sr = librosa.load(f'{filepaths}.wav', sr=None, mono=False)  # Load an audio file with librosa.
+  slicer = Slicer(
+      sr=sr,
+      threshold=-40,
+      min_length=2000,
+      min_interval=300,
+      hop_size=10,
+      max_sil_kept=500
+  )
+  chunks = slicer.slice(audio)
+  for i, chunk in enumerate(chunks):
+      if len(chunk.shape) > 1:
+          chunk = chunk.T  # Swap axes if the audio is stereo.
+      soundfile.write(f'{save_dir}/{a}_{i}.wav', chunk, sr)  # Save sliced audio files with soundfile.
 
-    i=0
-    for file_idx, filepath in enumerate(filepaths):
-
-        save_path = Path(save_dir)
-        save_path.mkdir(exist_ok=True, parents=True)
-
-        print(f"Transcribing file {file_idx}: '{filepath}' to segments...")
-        result = model.transcribe(filepath, word_timestamps=True, task="transcribe", beam_size=5, best_of=5)
-        segments = result['segments']
-
-        wav, sr = librosa.load(filepath, sr=None, offset=0, duration=None, mono=True)
-        wav, _ = librosa.effects.trim(wav, top_db=20)
-        peak = np.abs(wav).max()
-        if peak > 1.0:
-            wav = 0.98 * wav / peak
-        wav2 = librosa.resample(wav, orig_sr=sr, target_sr=out_sr)
-        wav2 /= max(wav2.max(), -wav2.min())
-
-        for i, seg in enumerate(segments):
-            start_time = seg['start']
-            end_time = seg['end']
-            wav_seg = wav2[int(start_time * out_sr):int(end_time * out_sr)]
-            wav_seg_name = f"{a}_{i}.wav" # 修改名字
-            i+=1
-            out_fpath = save_path / wav_seg_name
-            wavfile.write(out_fpath, rate=out_sr, data=(wav_seg * np.iinfo(np.int16).max).astype(np.int16))
+  if os.path.exists(f'{filepaths}'):  # 如果文件存在
+      os.remove(f'{filepaths}')  
 
 def transcribe_one(audio_path): # 使用whisper语音识别
     # load audio and pad/trim it to fit 30 seconds
@@ -60,11 +52,12 @@ if __name__ == '__main__':
     whisper_size = "medium"
     model = whisper.load_model(whisper_size)
     audio_path = f"./raw/{a}"
+    
     if os.path.exists(audio_path):
 	    for filename in os.listdir(audio_path): # 删除原来的音频和文本
 	        file_path = os.path.join(audio_path, filename)
 	        os.remove(file_path)
-    split_long_audio(model, f"data/{a}", f"./raw/{a}")
+    split_long_audio(f"data/{a}", f"./raw/{a}")
     files=os.listdir(audio_path)
     file_list_sorted = sorted(files, key=lambda x: int(os.path.splitext(x)[0].split('_')[1]))
     filepaths=[os.path.join(audio_path,i)  for i in file_list_sorted]
